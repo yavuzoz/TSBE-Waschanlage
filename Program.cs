32bc42d22780d55dd548c87231441657f1931621
Yavuz Özbay <ozbayavz@gmail.com>
2024-01-14 15:46:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TSBE_Waschanlage
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Fahrzeug fahrzeug1 = new Fahrzeug() { Bezeichnung = "Hippie Bus", Verschmutzungsgrad = 100 };
+			Fahrzeug fahrzeug2 = new Fahrzeug() { Bezeichnung = "AMG GT", Verschmutzungsgrad = 10 };
+			Fahrzeug fahrzeug3 = new Fahrzeug() { Bezeichnung = "Tesla", Verschmutzungsgrad = 50 };
+
+			Waschanlage waschanlage = new Waschanlage(3);
+
+			fahrzeug1.Anzeige();
+			waschanlage.WascheFahrzeug(fahrzeug1, 1, "S");
+			fahrzeug1 .Anzeige();
+
+			Thread.Sleep(31000);
+			fahrzeug2.Anzeige();
+			waschanlage.WascheFahrzeug(fahrzeug2, 1, "M");
+			fahrzeug2.Anzeige();
+
+			Thread.Sleep(31000);
+			fahrzeug3.Anzeige();
+			waschanlage.WascheFahrzeug(fahrzeug3, 1, "L");
+			fahrzeug3.Anzeige();
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Waschanlage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TSBE_Waschanlage
+{
+	internal class Waschanlage
+	{
+		private int anzahlWaschplätze;
+		private DateTime[] waschplätze;
+
+		public int AnzahlWaschplätze
+		{
+			get { return anzahlWaschplätze; }
+		}
+
+		public Waschanlage(int anzahlWaschplätze)
+		{
+			this.anzahlWaschplätze = anzahlWaschplätze;
+			waschplätze = new DateTime[anzahlWaschplätze];
+		}
+
+		public void WascheFahrzeug(Fahrzeug fahrzeug, int waschplatz, string waschprogramm)
+		{
+			if (waschplatz < 1 || waschplatz > anzahlWaschplätze)
+			{
+				Console.WriteLine("Ungültiger Waschplatz");
+			}
+			else
+			{
+				if (waschprogramm == "S" || waschprogramm == "M" || waschprogramm == "L")
+				{
+					if (waschplätze[waschplatz - 1] < DateTime.Now)
+					{
+						int dauer = 0;
+						if (waschprogramm == "S")
+						{
+							dauer = 30;
+							fahrzeug.Verschmutzungsgrad -= 50;
+						}
+						else if (waschprogramm == "M")
+						{
+							dauer = 60;
+							fahrzeug.Verschmutzungsgrad -= 80;
+						}
+						else if (waschprogramm == "L")
+						{
+							dauer = 90;
+							fahrzeug.Verschmutzungsgrad -= 100;
+						}
+						else
+						{
+							Console.WriteLine("Der Waschplatz ist leider schon belegt");
+						}
+
+						waschplätze[waschplatz - 1] = DateTime.Now.AddSeconds(dauer);
+						Console.WriteLine($"Ihr Fahrzeug {fahrzeug.Bezeichnung} wird gewaschen ");
+					}
+					
+				}
+				else
+				{
+					Console.WriteLine("Ungültiges Waschprogramm");
+				}
+			}
+		}
+	}
+}
